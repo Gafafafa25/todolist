e1f647b10c71db10a2120187bade5d65da370aa9
name <gafarova.tasia@gmail.com>
2025-08-15 16:18:00 +0300

    first commit

--- public/index.js
+++ public/index.js
@@ -1,1 +1,26 @@
-console.log('Happy developing ✨')
+const todo = ["Walk the dog", "Water the plants", "Wash the dishes"]
+
+
+
+const addTodobutton = document.getElementById("addTodoBtn")
+const TODOLIST = document.getElementById("list")
+
+
+let liCounter = 0
+function createTodo (){
+    console.log("+")
+    liCounter++
+    const newLi = document.createElement("li")
+    newLi.id = "li" + liCounter;
+    // newLi.innerHTML = addTodoinput;
+    newLi.innerText = `${document.getElementById("todoName").value}`;
+
+    TODOLIST.appendChild(newLi)
+}
+
+
+document.querySelector("#addTodoBtn").addEventListener("click", () => {
+    createTodo()
+})
+
+
---
+++ server.js
@@ -0,0 +1,49 @@
+
+const express = require('express');
+const { Pool } = require('pg');
+
+
+
+import express from 'express';
+import pkg from 'pg';
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+const app = express();
+app.use(express.json());
+
+//save to db
+app.post('/addNewToDo', async (req, res) => {
+    const d = req.body;
+    try {
+        const result = await pool.query('INSERT INTO todolist VALUES(DEFAULT, $1)',
+            [d.li]);
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Database error');
+    }
+});
+
+// app.get('/users', async (req, res) => {
+//     try {
+//         const result = await pool.query('SELECT * FROM users');
+//         res.json(result.rows);
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send('Database error');
+//     }
+// });
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
+
+
